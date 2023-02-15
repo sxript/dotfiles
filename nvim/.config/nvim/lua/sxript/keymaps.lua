ad93ca2dec8d227e934c48faf1a63011be344f6f
Filip Adamovic <45206898+SXRIPT@users.noreply.github.com>
2023-02-15 13:01:00 +0100

    chore: change tmux keybinds

--- nvim/.config/nvim/lua/sxript/keymaps.lua
+++ nvim/.config/nvim/lua/sxript/keymaps.lua
@@ -7,6 +7,9 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
 
+vim.keymap.set("v", "<leader>y", "+y")
+vim.keymap.set("n", "<leader>y", "+y")
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "N", "Nzzzv")
