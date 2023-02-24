861d786ebf54b3f6b18035324ba198f69a98354c
sxript <45206898+sxript@users.noreply.github.com>
2023-02-24 21:58:57 +0100

    chore: update tmux conf and nvim colors

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -4,8 +4,15 @@ require("catppuccin").setup({
 	transparent_background = true,
 });
 
+require("tokyonight").setup({
+	transparent = true;
+    styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+    }
+});
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return
 end
-
